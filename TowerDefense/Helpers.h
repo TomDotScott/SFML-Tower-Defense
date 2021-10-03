9d05a332e2362bf8e3f256c7b97df68320015806
Tom Scott <48756858+TomDotScott@users.noreply.github.com>
2021-10-03 16:26:06 +0100

    Helpers namespace
    Helper functions were placed into a namespace for ease of use throughout the project

---
+++ TowerDefense/Helpers.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include <cmath>
+#include <limits>
+
+namespace td
+{
+	namespace helper_functions
+	{
+		inline float sqr_magnitude(const sf::Vector2f& v)
+		{
+			return v.x * v.x + v.y * v.y;
+		}
+
+		inline float magnitude(const sf::Vector2f& v)
+		{
+			return sqrtf(sqr_magnitude(v));
+		}
+
+		inline void normalise_vector(sf::Vector2f& v)
+		{
+			v /= magnitude(v);
+		}
+
+		inline bool approximately_equal(const float a, const float b)
+		{
+			return std::fabs(a - b) <= ((std::fabs(a) < std::fabs(b) ? std::fabs(b) : std::fabs(a)) * std::numeric_limits<float>::epsilon());
+		}
+
+		inline bool essentially_equal(const float a, const float b)
+		{
+			return std::fabs(a - b) <= ((std::fabs(a) > std::fabs(b) ? std::fabs(b) : std::fabs(a)) * std::numeric_limits<float>::epsilon());
+		}
+
+		inline bool definitely_greater_than(const float a, const float b)
+		{
+			return a - b > ((std::fabs(a) < std::fabs(b) ? std::fabs(b) : std::fabs(a)) * std::numeric_limits<float>::epsilon());
+		}
+
+		inline bool definitely_less_than(const float a, const float b)
+		{
+			return b - a > ((std::fabs(a) < std::fabs(b) ? std::fabs(b) : std::fabs(a)) * std::numeric_limits<float>::epsilon());
+		}
+	}
+}
